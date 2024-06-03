59c36c0a15b10c46c36e0c8face215f34eff061a
Samuel Granieri Martins - PrestServ <samuel.granieri.prestserv@petrobras.com.br>
2024-06-02 22:11:15 -0300

    cadastro passageiros

--- js/add-parceiro.js
+++ js/add-parceiro.js
@@ -1,19 +1,23 @@
-document.getElementById('parceiro-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const response = await fetch('http://localhost:3000/api/parceiros', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer seuTokenJWT'
-      },
-      body: JSON.stringify({ name })
+document.addEventListener('DOMContentLoaded', async () => {
+
+  const form_parceiro = document.getElementById('parceiro-form');
+
+  form_parceiro.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      const name = document.getElementById('name').value;
+      const response = await fetch('http://localhost:3000/api/parceiros', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name })
+      });
+      const result = await response.json();
+      if (response.ok) {
+        alert('Parceiro adicionado com sucesso!');
+      } else {
+        alert(`Erro: ${result.message}`);
+      }
     });
-    const result = await response.json();
-    if (response.ok) {
-      alert('Parceiro adicionado com sucesso!');
-    } else {
-      alert(`Erro: ${result.message}`);
-    }
-  });
+});
   
