cc8046320637953ef0e7533765895cb51ee61757
Samuel Granieri Martins - PrestServ <samuel.granieri.prestserv@petrobras.com.br>
2024-06-02 22:53:07 -0300

    cadastro passageiros

--- js/add-passageiros.js
+++ js/add-passageiros.js
@@ -7,13 +7,22 @@ document.addEventListener('DOMContentLoaded', async () => {
       event.preventDefault();
 
       const name = document.getElementById('name').value;
+      const cpf = document.getElementById('cpf').value;
+      const email = document.getElementById('email').value;
+      const telefone = document.getElementById('telefone').value;
+      const endereco = document.getElementById('endereco').value;
+      const numero = document.getElementById('numero').value;
+      const cidade = document.getElementById('cidade').value;
+      const estado = document.getElementById('estado').value;
+      const senha = document.getElementById('senha').value;
+      const confirmacao_senha = document.getElementById('confirmacao_senha').value;
       
       const response = await fetch('http://localhost:3000/api/passageiros/passageiros', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ name})
+        body: JSON.stringify({ name, cpf, email, telefone, endereco, numero, cidade, estado, senha, confirmacao_senha})
       });
       
       const result = await response.json();
